65e499be2229a46f9ad0f3b2c1f80264b9c88331
legalmentemama <142763393+legalmentemama@users.noreply.github.com>
2024-09-10 01:06:19 -0300

    Update scriptnormal.js

--- assets/js/scriptnormal.js
+++ assets/js/scriptnormal.js
@@ -7,7 +7,7 @@ window.innerWidth < 768 &&
         (document.getElementById("imprimir").style.display = "none"),
             (document.getElementById("navbar").style.display = "none"),
             (document.getElementById("infofinal").style.display = "none"),
-            (document.getElementById("isotipo").style.transform = "translateX(-55px) scale(0.6)"),
+            (document.getElementById("isotipo").style.transform = "translateX(-40px) scale(0.6)"),
             (document.getElementById("logosolo").style.transform = "translateX(+15px) scale(0.6)"),
             (document.getElementById("cellmama").style.marginTop = "0px");
     }),
